81975257e2f6bfe71105bc9bf99674cbcb51939f
Lirin <62457732+1951586@users.noreply.github.com>
2022-06-12 10:17:49 +0800

    update

--- micro-services/change-detection/src/main/java/com/remote/changeDetection/controllers/changeDetectionController.java
+++ micro-services/change-detection/src/main/java/com/remote/changeDetection/controllers/changeDetectionController.java
@@ -3,20 +3,21 @@ package com.remote.changeDetection.controllers;
 import com.remote.tools.utils.Result;
 import io.swagger.annotations.Api;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 @CrossOrigin("*")
 @RestController
 @RequestMapping("/api/changeDetection")
 @RefreshScope
-@Api(value="changeDetection",tags = "changeDetection")
+@Api(value = "changeDetection", tags = "changeDetection")
 public class changeDetectionController {
 
-    @RequestMapping(value = "/test",method = RequestMethod.GET)
-    public Result<String> test(){
+    @RequestMapping(value = "/test", method = RequestMethod.POST)
+    public Result<String> test(@RequestParam("file") List<MultipartFile> file) {
+        System.out.println(file.size());
         return Result.wrapSuccessfulResult("It's successful!");
     }
 }
